39cb3c7bd0cea54fba7a9a7758ab9d1c092c14fa
JesseWiers <jessewiers99@hotmail.com>
2024-04-24 19:56:52 +0200

    changed cogvlm

--- POPE/generate_cogvlm_answers.py
+++ POPE/generate_cogvlm_answers.py
@@ -19,6 +19,7 @@ def load_model_and_tokenizer(args):
     return model, tokenizer
 
 def process_questions(args, model, tokenizer):
+    
     results = []
     with open(args.input_file, 'r') as file:
         for line in file:
@@ -37,11 +38,13 @@ def process_questions(args, model, tokenizer):
                 'input_ids': input_by_model['input_ids'].unsqueeze(0).to(args.device),
                 'token_type_ids': input_by_model['token_type_ids'].unsqueeze(0).to(args.device),
                 'attention_mask': input_by_model['attention_mask'].unsqueeze(0).to(args.device),
-                'images': [input_by_model['images'][0].to(args.device).to(torch.bfloat16)],  
+                #'images': [input_by_model['images'][0].to(args.device).to(torch.bfloat16)],  
+                'images': [[input_by_model['images'][0].to(args.device).to(torch.bfloat16)]],
             }
             
             if 'cross_images' in input_by_model and input_by_model['cross_images']:
-                inputs['cross_images'] = [input_by_model['cross_images'][0].to(args.device).to(torch.bfloat16)]
+                #inputs['cross_images'] = [input_by_model['cross_images'][0].to(args.device).to(torch.bfloat16)]
+                inputs['cross_images'] = [[input_by_model['cross_images'][0].to(args.device).to(torch.bfloat16)]]
 
             gen_kwargs = {"max_length": 2048, "do_sample": False} 
 
@@ -61,9 +64,9 @@ def process_questions(args, model, tokenizer):
 
 def main():
     parser = argparse.ArgumentParser(description="Process questions using a conversational model with image context")
-    parser.add_argument("--input_file", type=str, required=True, help="Path to the input JSON file with questions and image names")
-    parser.add_argument("--output_file", type=str, required=True, help="Path to the output JSON file for storing answers")
-    parser.add_argument("--image_dir", type=str, default='/home/jwiers/POPE/data/val2014/', help="Directory where images are stored")
+    parser.add_argument('--input_file', type=str, default='output/coco/coco_pope_popular_10.json', help='Path to the input JSON file with questions and image names')
+    parser.add_argument('--output_file', type=str, default='output/coco/ans_files/coco_pope_popular_10_cogvlm_ans.json', help='Path to the output JSON file for storing answers')
+    parser.add_argument("--image_dir", type=str, default='data/val2014/', help="Directory where images are stored")
     parser.add_argument("--from_pretrained", type=str, default="THUDM/cogagent-chat-hf", help="Model identifier for pretrained model")
     parser.add_argument("--local_tokenizer", type=str, default="lmsys/vicuna-7b-v1.5", help="Tokenizer identifier")
     parser.add_argument("--quant", type=int, choices=[4], default=None, help="Enable quantization (4-bit)")

b7e8144407c40fde4d449f551a150e93b00866eb
Raoul Ritter <rritter@int6.local.snellius.surf.nl>
2024-04-25 15:36:17 +0200

    Update POPE Json

--- POPE/generate_cogvlm_answers.py
+++ POPE/generate_cogvlm_answers.py
@@ -64,10 +64,10 @@ def process_questions(args, model, tokenizer):
 
 def main():
     parser = argparse.ArgumentParser(description="Process questions using a conversational model with image context")
-    parser.add_argument('--input_file', type=str, default='output/coco/coco_pope_popular_10.json', help='Path to the input JSON file with questions and image names')
-    parser.add_argument('--output_file', type=str, default='output/coco/ans_files/coco_pope_popular_10_cogvlm_ans.json', help='Path to the output JSON file for storing answers')
+    parser.add_argument('--input_file', type=str, default='output/coco/coco_box.json', help='Path to the input JSON file with questions and image names')
+    parser.add_argument('--output_file', type=str, default='output/coco/ans_files/coco_box.json', help='Path to the output JSON file for storing answers')
     parser.add_argument("--image_dir", type=str, default='data/val2014/', help="Directory where images are stored")
-    parser.add_argument("--from_pretrained", type=str, default="THUDM/cogagent-chat-hf", help="Model identifier for pretrained model")
+    parser.add_argument("--from_pretrained", type=str, default="THUDM/cogvlm-grounding-generalist-hf", help="Model identifier for pretrained model")
     parser.add_argument("--local_tokenizer", type=str, default="lmsys/vicuna-7b-v1.5", help="Tokenizer identifier")
     parser.add_argument("--quant", type=int, choices=[4], default=None, help="Enable quantization (4-bit)")
     parser.add_argument("--bf16", action="store_true", help="Use bfloat16 precision")
